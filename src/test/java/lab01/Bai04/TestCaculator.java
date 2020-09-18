cc442da35a9dab6eee739ddabdbd88fb432ed1b6
nguyenvandai98 <dainvph09220@fpt.edu.vn>
2020-09-18 07:45:00 +0700

    lab01

--- src/test/java/lab01/Bai04/TestCaculator.java
+++ src/test/java/lab01/Bai04/TestCaculator.java
@@ -4,35 +4,58 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class TestCaculator {
-
+     static Caculator caculator = new Caculator();
     @Test
     public void testSumCase1(){
-        Caculator caculator = new Caculator();
         int kpMongMuon = 3;
         int kqThucTe = caculator.sum(6,-3);
         Assert.assertEquals(kpMongMuon,kqThucTe);
     }
     @Test
+    public void testSumCase2(){
+
+        int kpMongMuon = 9;
+        int kqThucTe = caculator.sum(6,3);
+        Assert.assertEquals(kpMongMuon,kqThucTe);
+    }
+    @Test
     public void testSubtractCase1(){
-        Caculator caculator = new Caculator();
+
         int kpMongMuon = 9;
         int kqThucTe = caculator.suntract(6,-3);
         Assert.assertEquals(kpMongMuon,kqThucTe);
     }
     @Test
+    public void testSubtractCase2(){
+
+        int kpMongMuon = 3;
+        int kqThucTe = caculator.suntract(6,3);
+        Assert.assertEquals(kpMongMuon,kqThucTe);
+    }
+    @Test
     public void testMultipleCase1(){
-        Caculator caculator = new Caculator();
+
         int kpMongMuon = 9;
         int kqThucTe = caculator.multiple(3,3);
         Assert.assertEquals(kpMongMuon,kqThucTe);
     }
     @Test
     public void testDivineCase1() throws Exception {
-        Caculator caculator = new Caculator();
+
         double kpMongMuon = 1.5;
         double kqThucTe = caculator.divide(3,2);
-
         Assert.assertEquals(kpMongMuon,kqThucTe, 0.3f);
     }
+    @Test
+    public void testDivineCaseFailure() throws Exception {
+        Caculator caculator = new Caculator();
+      try{
+          double kqThucTe = caculator.divide(3,0);
+      }catch (Exception e){
+          Assert.assertEquals("",e.getMessage());
+      }
+
+    }
+
 
 }
